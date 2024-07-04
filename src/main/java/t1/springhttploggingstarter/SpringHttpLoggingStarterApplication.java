f566939f1c65e9397a78151e51ba2661b6c849c5
nikita <7jay94@mail.ru>
2024-07-04 21:35:41 +0200

    add: add README.md file.

--- src/main/java/t1/springhttploggingstarter/SpringHttpLoggingStarterApplication.java
+++ src/main/java/t1/springhttploggingstarter/SpringHttpLoggingStarterApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringHttpLoggingStarterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringHttpLoggingStarterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHttpLoggingStarterApplication.class, args);
+    }
 
 }
