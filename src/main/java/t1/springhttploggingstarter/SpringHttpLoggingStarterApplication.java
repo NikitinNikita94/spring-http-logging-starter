2e299cc535c084890e532cc0b9d61d1fff02f2c1
nikita <7jay94@mail.ru>
2024-07-04 21:33:19 +0200

    init: initial new project spring-http-logging-starter.

---
+++ src/main/java/t1/springhttploggingstarter/SpringHttpLoggingStarterApplication.java
@@ -0,0 +1,13 @@
+package t1.springhttploggingstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHttpLoggingStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHttpLoggingStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/t1/springhttploggingstarter/SpringHttpLoggingStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package t1.springhttploggingstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHttpLoggingStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
